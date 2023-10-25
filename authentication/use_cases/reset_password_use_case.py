163361dd5f083370c855666b1ed5246e8b0ed94b
Herve Wilfried <hervewilfried0100@gmail.com>
2023-10-25 04:16:03 +0000

    feat: Enregitrement des fonctionnalites

--- authentication/use_cases/reset_password_use_case.py
+++ authentication/use_cases/reset_password_use_case.py
@@ -7,14 +7,17 @@ from authentication.use_cases.verify_otp_code_use_case import VerifyCodeOTPUseca
 class ResetPasswordUsecase:
     @staticmethod
     def execute(**kwargs):
+
+
         user_email = kwargs.get("email")
         password = kwargs.get("password")
         otp_code = kwargs.get("code")
 
         user = get_object_or_404(User, email=user_email)
+        user_id = user.id
         use_case = VerifyCodeOTPUsecase()
 
-        is_correct = use_case.execute(otp_code, user.id)
+        is_correct = use_case.execute(otp_code, user_id)
 
         if is_correct is False:
             raise CodeNotCorrectError
